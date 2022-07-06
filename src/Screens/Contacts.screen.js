38f61581f0486cc9ab5daa26234d449de516e5e5
Khikar-1997 <harutyunyan.xikar@bk.ru>
2022-07-06 18:45:50 +0400

    Add Contact List

--- src/Screens/Contacts.screen.js
+++ src/Screens/Contacts.screen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, StyleSheet, Button } from 'react-native';
 import { ADD_CONTACTS } from '../Constants/navigationRouts';
+import ContactsList from './Components/ContactList/ContactsList';
 
 const Contacts = ({ navigation }) => {
   return (
     <View style={styles.contactScreen}>
       <Button title="Add" onPress={() => navigation.navigate(ADD_CONTACTS)} />
+      <ContactsList />
     </View>
   );
 };
