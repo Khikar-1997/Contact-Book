462e703c5a3247abc56efb84864eecd77d2757c0
Maro Harutyunyan <maro@Maros-MacBook-Air.local>
2022-07-22 13:30:39 +0400

    refactor: Uuid is moved inside 'createAction' function.

--- src/Actions/contacts.js
+++ src/Actions/contacts.js
@@ -2,12 +2,11 @@ import { createAction } from '@reduxjs/toolkit';
 
 import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
-const id = uuidv4();
 
 export const addContact = createAction('CONTACT/ADD_CONTACT', contactInfo => {
   return {
     payload: {
-      id: id,
+      id: uuidv4(),
       ...contactInfo,
       favorites: false,
     },
